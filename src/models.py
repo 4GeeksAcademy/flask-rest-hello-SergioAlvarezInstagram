1d96e6e8f2b19ec158bb0371798f42200c96ccc8
Sergioalv15 <sergioalv961@gmail.com>
2025-10-16 19:39:55 +0000

    [Relaciones en python entre tablas hecha]

--- src/models.py
+++ src/models.py
@@ -7,15 +7,26 @@ class User(db.Model):
     __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
-    firstname: Mapped[str] = mapped_column(String(255), nullable=False)
-    lastname: Mapped[str] = mapped_column(String(255), nullable=False)
+    firstname: Mapped[str] = mapped_column(String(255))
+    lastname: Mapped[str] = mapped_column(String(255))
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    posts: Mapped[list['Post']] = relationship(back_populates='users')
+    comments: Mapped[list['Comment']] = relationship(back_populates='author')
+    followers: Mapped[list['Follower']] = relationship(back_populates='followed', foreign_keys='Follower.user_to_id')
+    following: Mapped[list['Follower']] = relationship(back_populates='follower', foreign_keys='Follower.user_from_id')
+    
+
 
 class Follower(db.Model):
     __tablename__ = 'follower'
     id: Mapped[int] = mapped_column(primary_key=True)
     user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
     user_to_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    follower: Mapped['User'] = relationship(back_populates='following', foreign_keys=user_from_id)
+    followed: Mapped['User'] = relationship(back_populates='followers', foreign_keys=user_to_id)
+
+    
+
 
 
 class Comment(db.Model):
@@ -23,12 +34,14 @@ class Comment(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, unique=True)
     comment_text: Mapped[str] = mapped_column(String(255))
     author_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
-    
+    author : Mapped['User'] = relationship(back_populates='comments')
+
 
 class Post(db.Model):
     __tablename__ = 'post'
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    users: Mapped['User'] = relationship(back_populates='posts')
     
     
 class Media(db.Model):
